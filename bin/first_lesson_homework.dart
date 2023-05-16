447429c8eea0125e511e8e568a737c3ff2715c82
sezimka123 <erali.sezim@gmail.com>
2023-05-16 14:26:24 +0600

    first commit

---
+++ bin/first_lesson_homework.dart
@@ -0,0 +1,7 @@
+import 'package:first_lesson_homework/first_lesson_homework.dart' as first_lesson_homework;
+
+void main(List<String> arguments) {
+  print('Hello everyone!');
+  print('My name is Sezim. Nice to meet you.');
+  print("I'm gonna be cool programmer, really excited to learn.");
+}
---
+++ test/first_lesson_homework_test.dart
@@ -0,0 +1,8 @@
+import 'package:first_lesson_homework/first_lesson_homework.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
